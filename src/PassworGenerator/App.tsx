a6592c65b4e20b0ed88c89ae4f08171ae0e16e2f
marcos <marcosmendezser1@gnail.com>
2023-11-08 18:01:22 +0100

    Form

--- src/PassworGenerator/App.tsx
+++ src/PassworGenerator/App.tsx
@@ -1,14 +1,61 @@
+import { useState } from "react";
 import generatePassword from "./Functions/generatePassword";
 
 const PasswordApp = () => {
-  const handleClick = () => {
-    generatePassword(10, true, true, false);
+  const [lengthPassword, setLengthPassword] = useState(8);
+  const [checkCaptialLetters, setCapitalLetters] = useState(false);
+  const [checkNumbers, setCheckNumbers] = useState(false);
+  const [checkSymbols, setCheckSymbols] = useState(false);
+
+  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    generatePassword(
+      lengthPassword,
+      checkCaptialLetters,
+      checkNumbers,
+      checkSymbols
+    );
   };
   return (
     <>
       <h1>Password Generator</h1>
+      <form onSubmit={handleClick}>
+        <label>Number</label>
+        <input
+          type="number"
+          value={lengthPassword}
+          min={8}
+          max={16}
+          onChange={(e) => setLengthPassword(Number(e.target.value))}
+        />
+        <label>
+          Checkbox Capital Letters:
+          <input
+            type="checkbox"
+            checked={checkCaptialLetters}
+            onChange={(e) => setCapitalLetters(e.target.checked)}
+          />
+        </label>
 
-      <button onClick={handleClick}>GENERATE NEW PASSWORD</button>
+        <label>
+          Checkbox Numbers
+          <input
+            type="checkbox"
+            checked={checkNumbers}
+            onChange={(e) => setCheckNumbers(e.target.checked)}
+          />
+        </label>
+        <label>
+          Checkbox Symbols
+          <input
+            type="checkbox"
+            checked={checkSymbols}
+            onChange={(e) => setCheckSymbols(e.target.checked)}
+          />
+        </label>
+        <button type="submit">GENERATE NEW PASSWORD</button>
+      </form>
     </>
   );
 };
